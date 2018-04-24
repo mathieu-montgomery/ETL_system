e0d4490db51674444b95c1645fb4d4a3a972ecbd
Mallé <269384@via.dk>
2018-04-24 13:55:22 +0200

    taflighttables

---
+++ TaFlights/ETL_Extract_Taflights_Create_tables.sql
@@ -0,0 +1,67 @@
+drop table TaFlights;
+drop table TaFlightsNew;
+drop table TaFlightsYesterday;
+
+
+create table TaFlights
+	(	
+	 LAUNCHTIME 		date not null
+	,LANDINGTIME 		date 
+	,PLANEREGISTRATION 	char(03) not null;
+	,PILOT1INIT 		char(04) not null;
+	,PILOT2INIT 		char(01)
+	,LAUNCHAEROTOW 		char(01)
+						constraint coCHLaunchAerotow check (LaunchAerotow in ('Y' , 'N'))
+	,LAUNCHWINCH 		char(01)
+						constraint coCHLaunchWinch  check (LaunchWinch in ('Y' , 'N'))
+	,LAUNCHSELFLAUNCH 	char(01)
+						constraint coCHLaunchSelfLaunch check (LaunchSelfLaunch in ('Y' , 'N'))
+	,CABLEBREAK 		char(01)
+						constraint coCHCableBreak check (CableBreak in ('Y' , 'N'))
+	,CROSSCOUNTRYKM 	number(04)
+						default 0  
+	, CLUB				varchar(50) not null
+	
+	);
+	
+create table TaFlightsNew
+	(	
+	 LAUNCHTIME 		date not null
+	,LANDINGTIME 		date 
+	,PLANEREGISTRATION 	char(03) not null;
+	,PILOT1INIT 		char(04) not null;
+	,PILOT2INIT 		char(01)
+	,LAUNCHAEROTOW 		char(01)
+						constraint coCHLaunchAerotow check (LaunchAerotow in ('Y' , 'N'))
+	,LAUNCHWINCH 		char(01)
+						constraint coCHLaunchWinch  check (LaunchWinch in ('Y' , 'N'))
+	,LAUNCHSELFLAUNCH 	char(01)
+						constraint coCHLaunchSelfLaunch check (LaunchSelfLaunch in ('Y' , 'N'))
+	,CABLEBREAK 		char(01)
+						constraint coCHCableBreak check (CableBreak in ('Y' , 'N'))
+	,CROSSCOUNTRYKM 	number(04)
+						default 0  
+	, CLUB				varchar(50) not null
+	
+	);
+	
+create table TaFlightsYesterday
+	(	
+	 LAUNCHTIME 		date not null
+	,LANDINGTIME 		date 
+	,PLANEREGISTRATION 	char(03) not null;
+	,PILOT1INIT 		char(04) not null;
+	,PILOT2INIT 		char(01)
+	,LAUNCHAEROTOW 		char(01)
+						constraint coCHLaunchAerotow check (LaunchAerotow in ('Y' , 'N'))
+	,LAUNCHWINCH 		char(01)
+						constraint coCHLaunchWinch  check (LaunchWinch in ('Y' , 'N'))
+	,LAUNCHSELFLAUNCH 	char(01)
+						constraint coCHLaunchSelfLaunch check (LaunchSelfLaunch in ('Y' , 'N'))
+	,CABLEBREAK 		char(01)
+						constraint coCHCableBreak check (CableBreak in ('Y' , 'N'))
+	,CROSSCOUNTRYKM 	number(04)
+						default 0  
+	, CLUB				varchar(50) not null
+	
+	);
